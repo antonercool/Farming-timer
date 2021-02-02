cd6b4a2bdd5e25c288dcefd9ea22c4a1cce09207
antonercool <antonsihm@gmail.com>
2021-02-02 09:21:10 +0100

    intial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Farming-timer
 Farming timer for maplestory
+
+
+Prereqs:
+install python3
+install pip
+
+Libs:
+- pip install pygame 
+
+how to run:
+default: 90 farming - 30 looting
+./farmer.py                    
+
+custom:
+./farmer.py 90 20
---
+++ farmer.py
@@ -0,0 +1,26 @@
+from pygame import mixer  # Load the popular external library
+import time
+import sys
+
+if __name__ == '__main__': 
+    
+    farming_period = 90
+    loot_period = 30
+
+    if len(sys.argv) == 2:
+        farming_period = sys.argv[1]
+        loot_period = sys.argv[2] 
+    
+    mixer.init()
+    
+    try:
+        while True:
+            time.sleep(farming_period)
+            mixer.music.load('Shounds/monney.mp3')
+            mixer.music.play() 
+            time.sleep(loot_period-3)
+            
+    except KeyboardInterrupt:
+        pass
+    
+ 
